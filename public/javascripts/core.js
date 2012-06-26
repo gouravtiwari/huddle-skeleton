c7463096daf86d93163bec01de7340b7ac61e6d6
Zhengyang Feng <zhengyang.feng@nyu.edu>
2012-06-26 17:33:49 -0400

    refine functions in core.js

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,60 +1,15 @@
-var generatedNumbersOnPage = [];
-
 if (!window.console) console = {log: function() {}};
 
 function getRandomImageId(startNum, endNum) {
   return Math.floor(startNum+(Math.random()*(endNum-startNum)));
 }
 
-function getRandomImagePath(startNum, endNum) {
-  var number = getRandomImageId(startNum, endNum);
-
-  if(generatedNumbersOnPage.length == 0) {
-    generatedNumbersOnPage.push(number);
-  }
-  else if(generatedNumbersOnPage.length == 1) {
-    while(number == generatedNumbersOnPage[0]) {
-      number = getRandomImageId(startNum, endNum);    
-    }
-    generatedNumbersOnPage.push(number);
-  }
-  else if(generatedNumbersOnPage.length == 2) {
-    generatedNumbersOnPage = [];
-    generatedNumbersOnPage.push(number);  
-  }
-
-  return getImagePath(number);
-}
-
-function getImageNumbersOnPage() {
-  return generatedNumbersOnPage;
-}
-
 function getImagePath(imageNum) {
-  return "/images/"+imageNum+".jpg";  
-}
-
-function allGeneratedImageNumbers() {
-  console.log(generatedNumbers);
-  $.unique(generatedNumbers);
-}
-
-function sortObjectOnKeys(object) {
-  var sortedObject = {},
-  key, keys = [];
-
-  keys = allKeys(object)
-  keys.sort();
-  keys.reverse();
-
-  for (key = 0; key < keys.length; key++) {
-    sortedObject[keys[key]] = object[keys[key]];
-  }
-  return sortedObject;
+  return "images/"+imageNum+".jpg";  
 }
 
 function allKeys(object) {
-  var key, 
+  var key; 
   keys = [];
 
   for (key in object) {
@@ -66,8 +21,8 @@ function allKeys(object) {
 }
 
 function diffArrays(firstArr, secondArr) {
-console.log(firstArr)
-console.log(secondArr)
+  console.log(firstArr);
+  console.log(secondArr);
   var strA = ":" + firstArr.join("::") + ":";
   var strB = ":" +  secondArr.join(":|:") + ":";
 
@@ -76,4 +31,10 @@ console.log(secondArr)
   var arrDiff = strDiff.split("::");
 
   return arrDiff;
+}
+
+function getURLParameter(name) {
+  return decodeURI(
+    (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+  );
 }
